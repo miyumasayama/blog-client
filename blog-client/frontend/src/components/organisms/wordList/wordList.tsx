61cd8b5fa98d8d7e8968f093d9b80dc365528eb8
miyu-masayama <masayama.miyu@i3design.co.jp>
2023-09-14 17:43:00 +0900

    feat/create機能追加

--- blog-client/frontend/src/components/organisms/wordList/wordList.tsx
+++ blog-client/frontend/src/components/organisms/wordList/wordList.tsx
@@ -1,24 +1,37 @@
+import { Box, Button, Stack } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { FC } from "react";
-import { useListWordsQuery } from "../../../reducers/appApis";
+import { useCreateWordMutation, useListWordsQuery } from "../../../reducers/appApis";
 
 export const WordList: FC = () => {
-  const { data } = useListWordsQuery();
+  const { data, refetch } = useListWordsQuery();
+  const [createWord] = useCreateWordMutation();
+  const handleCreateWord = async () => {
+    const data = await createWord({ title: "言葉", definition: "ひとのことば" });
+    void refetch();
+  };
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <DataGrid
-        columns={columns}
-        rows={data?.post ?? []}
-        rowSelection
-        pageSizeOptions={[100]}
-        sx={{ maxWidth: "1000px" }}
-      />
-    </div>
+    <Stack gap={2}>
+      <Box component='div' display='flex' justifyContent='flex-end'>
+        <Button variant='contained' onClick={() => handleCreateWord()}>
+          create
+        </Button>
+      </Box>
+      <Box component='div' display='flex' justifyContent='center' width='100%'>
+        <DataGrid
+          columns={columns}
+          rows={data ?? []}
+          rowSelection
+          pageSizeOptions={[100]}
+          sx={{ maxWidth: "1000px", height: "90%" }}
+        />
+      </Box>
+    </Stack>
   );
 };
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", sortable: true, flex: 1 },
   { field: "title", headerName: "word", sortable: true, flex: 2 },
-  { field: "content", headerName: "definition", sortable: true, flex: 3 },
+  { field: "definition", headerName: "definition", sortable: true, flex: 3 },
 ];
--- blog-client/frontend/src/reducers/appApis/injections/wordsApi.ts
+++ blog-client/frontend/src/reducers/appApis/injections/wordsApi.ts
@@ -1,11 +1,19 @@
 import { baseApi } from "../baseApi";
 
-type ListWordsResponse = {
-  post: {
-    id: number;
-    title: string;
-    content: string;
-  }[];
+type Word = {
+  id: number;
+  title: string;
+  definition: string;
+  example?: string;
+};
+
+type ListWordsResponse = Word[];
+
+type CreateWordResponse = Word;
+
+type CreateWordParams = {
+  title: string;
+  definition: string;
 };
 
 export const wordsApi = baseApi.injectEndpoints({
@@ -16,7 +24,17 @@ export const wordsApi = baseApi.injectEndpoints({
         method: "GET",
       }),
     }),
+    createWord: builder.mutation<CreateWordResponse, CreateWordParams>({
+      query: (params) => ({
+        url: "words",
+        method: "POST",
+        body: {
+          title: params.title,
+          definition: params.definition,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useListWordsQuery } = wordsApi;
+export const { useListWordsQuery, useCreateWordMutation } = wordsApi;
